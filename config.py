434b677fbc3c6c6a40cf00d95b9ff2251e5840c2
Average Wizard <thelordofgiggles@gmail.com>
2016-01-09 15:02:15 -0700

    I don't even know what I changed

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ SCREEN_TITLE = "Game Template"
 # If true, default width and height will be ignored and overridden
 SCREEN_FULLSCREEN = False
 
-FRAMERATE = 60
+FRAMERATE = 120
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ sys.dont_write_bytecode = True
 from base import *
 
 class Game(Base):
-	def __init__(self, title, width, height, framerate, fullscreen):
-		Base.__init__(self, title, width, height, framerate, fullscreen)
+	def __init__(self):
+		Base.__init__(self, config.SCREEN_TITLE, config.SCREEN_WIDTH, config.SCREEN_HEIGHT, config.FRAMERATE, config.SCREEN_FULLSCREEN)
 		self.loadFolders(images=True)
 
 	def logic(self, keys, newkeys, buttons, newbuttons, mousepos, lastmousepos, delta):
@@ -16,5 +16,5 @@ class Game(Base):
 
 
 if __name__ == "__main__":
-	g = Game(config.SCREEN_TITLE, config.SCREEN_WIDTH, config.SCREEN_HEIGHT, config.FRAMERATE, config.SCREEN_FULLSCREEN)
+	g = Game()
 	g.main()
